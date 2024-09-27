47f1d6cbb34d75c958bae247c604afdf6e38fba0
Kattily-eng <113124949+Kattily-eng@users.noreply.github.com>
2024-09-27 09:47:14 -0600

    Update and rename Star.jave to Star.java

---
+++ app/src/main/java/com/example/nebula/Star.java
@@ -0,0 +1,25 @@
+package com.example.nebula;
+
+public class java extends Shape {
+
+    public Star(int x, int y){
+        super(x, y);
+    }
+private String color = "bright yellow";
+
+    public int GetX() {
+        return x;
+    }
+
+    public int GetY() {
+        return y;
+    }
+
+    public void SetX(int x) {
+        this.x = x;
+    }
+
+    public void SetY(int y) {
+        this.y = y;
+    }
+}
